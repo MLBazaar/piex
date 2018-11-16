6942d1617e37dc0e157e1527c7dbf299fb8edb8c
Carles Sala <carles@pythiac.com>
2018-11-16 18:20:33 +0100

    Cleanup the public API

--- piex/explorer.py
+++ piex/explorer.py
@@ -11,14 +11,14 @@ import boto3
 import pandas as pd
 from mlblocks import MLPipeline
 
-from piex import scoring
+from piex.scoring import pipeline_dataset_score
 
 LOGGER = logging.getLogger(__name__)
 
-DATASETS_COLUMNS = ['dataset', 'data_modality', 'task_type', 'task_subtype']
-
 
 class PipelineExplorer:
+
+    DATASETS_COLUMNS = ['dataset', 'data_modality', 'task_type', 'task_subtype']
 
     def __init__(self, data_path='data'):
         self.data_path = data_path
@@ -29,114 +29,84 @@ class PipelineExplorer:
 
         self.dfs = dict()
 
-    def get_table(self, table_name):
+    def get_datasets(self, **filters):
+        """Get a pandas DataFrame with information about the available datasets."""
         raise NotImplementedError
 
-    def load_table(self, table_name):
-        df = self.dfs.get(table_name)
-        if df is not None:
-            return df
-
-        pkl_filename = os.path.join(self.csvs_path, table_name + '.pkl')
-        if os.path.exists(pkl_filename):
-            with open(pkl_filename, 'rb') as pkl_file:
-                df = pickle.load(pkl_file)
-
-        else:
-            df = self.get_table(table_name)
-            with open(pkl_filename, 'wb') as pkl_file:
-                pickle.dump(df, pkl_file)
-
-        self.dfs[table_name] = df
-
-        return df
-
     def get_tests(self, **filters):
-        ddf = self.load_table('datasets')
-        tdf = self.load_table('tests')
-        tdf = tdf.merge(ddf, how='left', on='dataset')
-        return tdf.loc[(tdf[list(filters)] == pd.Series(filters)).all(axis=1)].copy()
+        """Get a pandas DataFrame with information about the executed tests."""
+        raise NotImplementedError
 
     def get_test_results(self, **filters):
-        rdf = self.load_table('test_results')
-        rdf = rdf.loc[(rdf[list(filters)] == pd.Series(filters)).all(axis=1)]
-        tdf = self.load_table('tests')
-        return rdf.merge(tdf, how='left', on=['dataset', 'test_id'], suffixes=('', '_results'))
+        """Get a pandas DataFrame with the results of the executed tests."""
+        raise NotImplementedError
 
     def get_pipelines(self, **filters):
-        df = self.load_table('pipelines')
-        df['pipeline'] = df['name']
-        return df.loc[(df[list(filters)] == pd.Series(filters)).all(axis=1)].copy()
-
-    def get_templates(self, **filters):
-        df = self.load_table('solutions')
-        df['pipeline'] = df['name']
-        return df.loc[(df[list(filters)] == pd.Series(filters)).all(axis=1)].copy()
-
-    def get_datasets(self, with_tests=True, **filters):
-        if with_tests:
-            tests = self.get_tests(**filters).reindex(columns=DATASETS_COLUMNS)
-            subset= ['task_type']
-            tests = tests.sort_values(DATASETS_COLUMNS).dropna(subset=subset)
-            return tests.drop_duplicates().reset_index(drop=True)
-        else:
-            return self.load_table('datasets')
-
-    def get_dataset_id(self, dataset):
-        datasets = self.load_table('datasets')
-        dataset = datasets[datasets.dataset == dataset]
-        if not dataset.empty:
-            return dataset.iloc[0].dataset_id
+        """Get a pandas DataFrame with information about the scored pipelines."""
+        raise NotImplementedError
 
     def get_best_pipeline(self, dataset):
-        sdf = self.load_table('solutions')
-        dsdf = sdf[sdf.dataset == dataset]
-        if dsdf.empty:
-            dataset = self.get_dataset_id(dataset)
+        """Get information about the best pipeline ever found for the given dataset."""
+        raise NotImplementedError
 
-        dsdf = sdf[sdf.dataset == dataset]
-        if not dsdf.empty:
-            return dsdf.rename({'_id': 'id'}).sort_values('rank').iloc[0]
+    def get_templates(self, **filters):
+        """Get a pandas DataFrame with information about the available templates."""
+        raise NotImplementedError
 
-    def get_best_template(self, dataset):
-        return self.get_best_pipeline(dataset)['name']
+    def get_dataset_id(self, dataset):
+        """Get the TRAIN dataset id for a given dataset."""
+        raise NotImplementedError
 
     def load_pipeline(self, pipeline_id):
+        """Get the dict representation of the pipeline."""
         raise NotImplementedError
 
     def load_template(self, template_name):
+        """Get the dict representation of the template."""
         raise NotImplementedError
 
     def load_best_pipeline(self, dataset):
+        """Get the dict representation of the best pipeline ever found for this dataset."""
         pipeline = self.get_best_pipeline(dataset)
-        return self.load_pipeline(pipeline._id)
+        return self.load_pipeline(pipeline.id)
+
+    def get_best_template(self, dataset):
+        """Get the name of the template that got the best score for this dataset."""
+        pipeline = self.get_best_pipeline(dataset)
+        return pipeline['name']
+
+    def load_best_template(self, dataset):
+        """Get the dict representation of the best template ever found for this dataset."""
+        template_name = self.get_best_template(dataset)
+        return self.load_template(template_name)
 
     def get_default_hyperparameters(self, template_name):
+        """Get the default hyperparmeters of the given template."""
         template = self.load_template(template_name)
         mlpipeline = MLPipeline.from_dict(template)
         return mlpipeline.get_hyperparameters()
 
     def get_tunable_hyperparameters(self, template_name):
+        """Get the tunable hyperparmeters of the given template."""
         template = self.load_template(template_name)
         mlpipeline = MLPipeline.from_dict(template)
         return mlpipeline.get_tunable_hyperparameters()
 
-    def test_pipeline(self, template_name, dataset, hyperparameters=None,
-                      n_splits=5, cv=None, random_state=0):
+    def score_pipeline(self, pipeline_id, dataset, n_splits=5, cv=None, random_state=0):
+        """Cross validate the given pipeline on this dataset."""
+        pipeline = self.load_pipeline(pipeline_id)
+        return pipeline_dataset_score(pipeline, dataset, n_splits, cv, random_state)
+
+    def score_template(self, template_name, dataset, hyperparameters=None,
+                       n_splits=5, cv=None, random_state=0):
+        """Cross validate the given template on this dataset."""
 
         template = self.load_template(template_name)
         if hyperparameters is None:
             hyperparameters = self.get_default_hyperparameters(template_name)
 
         template['hyperparameters'] = hyperparameters
-
-        return scoring.pipeline_dataset_score(
-            template,
-            dataset,
-            n_splits,
-            cv,
-            random_state
-        )
+        return pipeline_dataset_score(template, dataset, n_splits, cv, random_state)
 
 
 class MongoPipelineExplorer(PipelineExplorer):
@@ -145,23 +115,24 @@ class MongoPipelineExplorer(PipelineExplorer):
         super().__init__(*args, **kwargs)
         self.db = db
 
-    def get_collection(self, collection, query=None, project=None):
+    def _get_collection(self, collection, query=None, project=None):
         cursor = self.db[collection].find(query or dict(), project)
         return pd.DataFrame(list(cursor))
 
     def get_datasets(self, **filters):
-        return self.get_collection('datasets', filters)
+        df = self._get_collection('datasets', filters).reindex(columns=self.DATASETS_COLUMNS)
+        return df.sort_values(self.DATASETS_COLUMNS).dropna(subset=['task_type'])
 
     def get_tests(self, **filters):
-        ddf = self.get_collection('datasets', filters, {'_id': 0})
+        ddf = self._get_collection('datasets', filters, {'_id': 0})
         datasets = list(ddf.dataset.unique())
-        tdf = self.get_collection('tests', {'dataset': {'$in': datasets}}, {'_id': 0})
+        tdf = self._get_collection('tests', {'dataset': {'$in': datasets}}, {'_id': 0})
         return tdf.loc[(tdf[list(filters)] == pd.Series(filters)).all(axis=1)].copy()
 
     def get_test_results(self, **filters):
-        rdf = self.get_collection('test_results', filters, {'_id': 0})
+        rdf = self._get_collection('test_results', filters, {'_id': 0})
         test_ids = list(rdf.test_id.unique())
-        tdf = self.get_collection('tests', {'test_id': {'$in': test_ids}}, {'_id': 0})
+        tdf = self._get_collection('tests', {'test_id': {'$in': test_ids}}, {'_id': 0})
         return rdf.merge(tdf, how='left', on=['dataset', 'test_id'], suffixes=('', '_results'))
 
     def get_pipelines(self, **filters):
@@ -175,7 +146,7 @@ class MongoPipelineExplorer(PipelineExplorer):
             'score', 'template', 'test_id', 'pipeline'
         ]
 
-        df = self.get_collection('solutions', filters, project)
+        df = self._get_collection('solutions', filters, project)
         df['pipeline'] = df['name']
         loader = df.pop('loader')
         df['data_modality'] = loader.apply(lambda l: l.get('data_modality'))
@@ -204,11 +175,34 @@ class MongoPipelineExplorer(PipelineExplorer):
             best = self.get_pipelines(_id=best[0]['_id']).iloc[0]
             return best.rename({'_id': 'id'})
 
+    def get_templates(self, **filters):
+        templates = list()
+        project = [
+            '_id',
+            'metadata',
+            'primitives',
+        ]
+
+        for template in self.db.pipelines.find({}, project):
+            template.update(template.pop('metadata'))
+            template['id'] = str(template.pop('_id'))
+            templates.append(template)
+
+        tdf = pd.DataFrame(templates)
+        return tdf[[
+            'id',
+            'name',
+            'data_type',
+            'task_type',
+            'primitives',
+            'insert_ts'
+        ]]
+
     def load_pipeline(self, pipeline_id):
         return self.db.solutions.find_one({'_id': pipeline_id})
 
     def load_template(self, template_name):
-        return self.db.pipelines.find_one({'metadata.name': template_name})
+        return self.db.templates.find_one({'metadata.name': template_name})
 
 
 class S3PipelineExplorer(PipelineExplorer):
@@ -217,7 +211,7 @@ class S3PipelineExplorer(PipelineExplorer):
         super().__init__(*args, **kwargs)
         self.bucket = bucket
 
-    def get_table(self, table_name):
+    def _get_table(self, table_name):
         LOGGER.info("Downloading %s csv from S3", table_name)
         key = os.path.join('csvs', table_name + '.csv.gz')
 
@@ -229,8 +223,27 @@ class S3PipelineExplorer(PipelineExplorer):
 
         return pd.read_csv(gzip_file)
 
-    def get_json(self, folder, object_id):
-        key = os.path.join(folder, object_id + '.json.gz')
+    def _load_table(self, table_name):
+        df = self.dfs.get(table_name)
+        if df is not None:
+            return df
+
+        pkl_filename = os.path.join(self.csvs_path, table_name + '.pkl')
+        if os.path.exists(pkl_filename):
+            with open(pkl_filename, 'rb') as pkl_file:
+                df = pickle.load(pkl_file)
+
+        else:
+            df = self._get_table(table_name)
+            with open(pkl_filename, 'wb') as pkl_file:
+                pickle.dump(df, pkl_file)
+
+        self.dfs[table_name] = df
+
+        return df
+
+    def _get_json(self, folder, pipeline_id):
+        key = os.path.join(folder, pipeline_id + '.json.gz')
         s3 = boto3.client('s3')
         obj = s3.get_object(Bucket=self.bucket, Key=key)
 
@@ -238,8 +251,49 @@ class S3PipelineExplorer(PipelineExplorer):
         gzip_file = gzip.GzipFile(fileobj=body_bytes, mode='rb')
         return json.load(gzip_file)
 
+    def get_tests(self, **filters):
+        ddf = self._load_table('datasets')
+        tdf = self._load_table('tests')
+        tdf = tdf.merge(ddf, how='left', on='dataset')
+        return tdf.loc[(tdf[list(filters)] == pd.Series(filters)).all(axis=1)].copy()
+
+    def get_test_results(self, **filters):
+        rdf = self._load_table('test_results')
+        return rdf.loc[(rdf[list(filters)] == pd.Series(filters)).all(axis=1)]
+
+    def get_pipelines(self, **filters):
+        df = self._load_table('pipelines')
+        df['pipeline'] = df['name']
+        return df.loc[(df[list(filters)] == pd.Series(filters)).all(axis=1)].copy()
+
+    def get_templates(self, **filters):
+        df = self._load_table('pipelines')
+        df['pipeline'] = df['name']
+        return df.loc[(df[list(filters)] == pd.Series(filters)).all(axis=1)].copy()
+
+    def get_datasets(self, **filters):
+        df = self._load_table('datasets').reindex(columns=self.DATASETS_COLUMNS)
+        df = df.loc[(df[list(filters)] == pd.Series(filters)).all(axis=1)].copy()
+        return df.sort_values(self.DATASETS_COLUMNS).dropna(subset=['task_type'])
+
+    def get_dataset_id(self, dataset):
+        datasets = self._load_table('datasets')
+        dataset = datasets[datasets.dataset == dataset]
+        if not dataset.empty:
+            return dataset.iloc[0].dataset_id
+
+    def get_best_pipeline(self, dataset):
+        sdf = self._load_table('pipelines')
+        dsdf = sdf[sdf.dataset == dataset]
+        if dsdf.empty:
+            dataset = self.get_dataset_id(dataset)
+
+        dsdf = sdf[sdf.dataset == dataset]
+        if not dsdf.empty:
+            return dsdf.rename(columns={'_id': 'id'}).sort_values('rank').iloc[0]
+
     def load_pipeline(self, pipeline_id):
-        return self.get_json('pipelines', pipeline_id)
+        return self._get_json('pipelines', pipeline_id)
 
     def load_template(self, template_name):
-        return self.get_json('templates', template_name.replace('/', '.'))
+        return self._get_json('templates', template_name.replace('/', '.'))
