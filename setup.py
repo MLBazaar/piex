be93b47765a17bcfff10eef0cb55243a1ba94318
Carles Sala <carles@pythiac.com>
2018-11-28 21:04:47 +0100

    Bump version: 0.1.1-dev → 0.1.1

--- setup.py
+++ setup.py
@@ -91,6 +91,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/piex',
-    version='0.1.1-dev',
+    version='0.1.1',
     zip_safe=False,
 )
